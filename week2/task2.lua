d0d2ce453e342a87b11623db6e3c7826637028fd
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2020-01-31 14:08:06 +0000

    Update task2.lua

--- week2/task2.lua
+++ week2/task2.lua
@@ -4,11 +4,11 @@ pwm.setup(pinDim,1000,dc)
 pwm.start(pinDim)
 mytimer = tmr.create()
 mytimer:alarm(200,tmr.ALARM_AUTO,function()
- dc=dc-10
- print(dc)
- pwm.setduty(pinDim,dc)
- if (dc<10) then
- dc=1023
- end
+    dc=dc-10
+    print(dc)
+    pwm.setduty(pinDim,dc)
+    if (dc<10) then
+        dc=1023
+    end
 end
 )
