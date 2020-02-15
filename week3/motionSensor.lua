19d8bdcccc123a8d919f952308a81726ab31e757
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2020-02-15 00:06:00 +0000

    updated

---
+++ week3/motionSensor.lua
@@ -0,0 +1,25 @@
+irPin = 1
+gpio.mode(irPin,gpio.INT)
+motionstate = 0
+print(gpio.read(irPin))
+ledPin = 4
+gpio.mode(ledPin,gpio.OUTPUT)
+gpio.write(ledPin,gpio.HIGH)
+mytimer = tmr:create()
+mytimer:register(300, 1, function()
+    level = gpio.read(irPin)
+    if motionstate == 0 then
+    print('working')
+    gpio.write(ledPin,gpio.HIGH)
+    else
+    motionstate = 0
+    end
+    if level == gpio.HIGH then
+    print('motion detected')
+    gpio.write(ledPin,gpio.LOW)
+    motionstate = 1
+    tmr.delay(5000000)
+    end
+end
+)
+mytimer:start()
