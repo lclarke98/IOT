3cd77798694dd4661cbe4a08c0728daeea075330
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2020-03-12 23:35:35 +0000

    week 9 started

---
+++ week9/init.lua
@@ -0,0 +1,16 @@
+HOST = "io.adafruit.com"
+PORT = 1883
+ADAFRUIT_IO_USERNAME = "lclarke98"
+ADAFRUIT_IO_KEY = "aio_PCVw59cFV4CDvll8a26WAzfGdjzT"
+m = mqtt.Client("Client1", 300, ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
+m:lwt("/lwt", "Now offline", 1, 0)
+m:on("connect", function(client)
+    print("Client connected")
+    print("MQTT client connected to" .. HOST)
+    client:subscribe(SUBSCRIBE_TOPIC, 1,
+                     function(client) print("Subscribe successfully") end)
+end)
+m:on("offline", function(client) print("Client offline") end)
+m:connect(HOST, PORT, false, false, function(conn) end, function(conn, reason)
+    print("ERROR: " .. reason)
+end)
