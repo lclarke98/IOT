986b4c36b77f48ebf5e794bc4754302a4ff3538d
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2020-03-13 13:44:10 +0000

    work done

--- week9/init.lua
+++ week9/init.lua
@@ -10,13 +10,7 @@ wifi.sta.connect()
 --wifi.sta.disconnect()
 --manual connect and disconnect
 
-wifi.setmode(wifi.SOFTAP)
-ap_cfg={}
-ap_cfg.ssid="test"
-ap_cfg.pwd="test1234"
---the rest have default value
---check them by yourself, which is helpful to know how it works - Dalin
-wifi.ap.config(ap_cfg)
+
 
 HOST = "io.adafruit.com"
 PORT = 1883
