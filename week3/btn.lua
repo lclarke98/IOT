301db23ff87d71fe0bc7aa7dae5618dc08d9953f
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2020-03-20 18:52:04 +0000

    work done

--- week3/btn.lua
+++ week3/btn.lua
@@ -23,3 +23,25 @@ end
 end)
 mytimer:start()
 
+dhtPin = 2
+buttonPin = 7
+gpio.mode(buttonPin, gpio.INPUT)
+gpio.write(buttonPin, gpio.HIGH)
+pushed = 0
+mytimer = tmr.create()
+
+mytimer:register(100, 1, function()
+    if gpio.read(buttonPin)==0 then
+        pushed = 1
+        status, temp, humi, temp_dec, humi_dec = dht.read11(dhtPin)
+        if status == dht.OK then
+            print("DHT Temperature:"..temp..";".."Humidity:"..humi)
+            elseif status == dht.ERROR_CHECKSUM then
+            print( "DHT Checksum error." )
+            elseif status == dht.ERROR_TIMEOUT then
+            print( "DHT timed out." )
+        end
+    end
+end)
+
+mytimer:start()
--- week3/init.lua
+++ week3/init.lua
@@ -1,22 +1,12 @@
 dhtPin = 2
-buttonPin = 7
-gpio.mode(buttonPin, gpio.INPUT)
-gpio.write(buttonPin, gpio.HIGH)
-pushed = 0
-mytimer = tmr.create()
+status, temp, humi, temp_dec, humi_dec = dht.read11(dhtPin)
+if status == dht.OK then
+    print("DHT Temperature:" .. temp .. ";" .. "Humidity:" .. humi)
+    -- 2 dots are used for concatenation
+elseif status == dht.ERROR_CHECKSUM then
+    print("DHT Checksum error.")
+elseif status == dht.ERROR_TIMEOUT then
+    print("DHT timed out.")
+end
+print("Temperature and Humidity Given")
 
-mytimer:register(100, 1, function()
-    if gpio.read(buttonPin)==0 then
-        pushed = 1
-        status, temp, humi, temp_dec, humi_dec = dht.read11(dhtPin)
-        if status == dht.OK then
-            print("DHT Temperature:"..temp..";".."Humidity:"..humi)
-            elseif status == dht.ERROR_CHECKSUM then
-            print( "DHT Checksum error." )
-            elseif status == dht.ERROR_TIMEOUT then
-            print( "DHT timed out." )
-        end
-    end
-end)
-
-mytimer:start()
