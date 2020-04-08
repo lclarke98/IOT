3da15fcf52987a74c724e890ef0da86ed60babd5
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2020-04-08 21:37:40 +0100

    moved to new repo

--- project/db.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE if not exists climateSensor;
-
-CREATE TABLE if not exists climateSensor.data(
-  weatherDataID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  datetime timestamp NOT NULL,
-  humidity VARCHAR(88) NOT NULL,
-  tempC VARCHAR(88) NOT NULL,
-)
--- project/server/api/index.js
+++
@@ -1,9 +0,0 @@
-  
-const express = require('express');
-const api = express.Router();
-const db = require('../db-function');
-const bodyParser = require('body-parser')
-module.exports = api;
-
-api.use(bodyParser.json());
-api.use(bodyParser.urlencoded({ extended: true }));
